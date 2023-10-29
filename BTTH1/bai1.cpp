dd77523b1ab82df302023926bac1bb9992a503c8
duy23it6 <148861035+duy23it6@users.noreply.github.com>
2023-10-29 20:52:05 +0700

    khoi tao du an

---
+++ BTTH1/bai1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("nhap so nguyen n:");
+    scanf("%d", &n);
+    if(n%2==0)
+    printf("n la so chan");
+    else
+    printf("n la so le");
+    return 0;
+}
